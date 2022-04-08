b8e1cb093e3863017ee2ae0fe28a7f8c34bd06ee
Emily Bozek <emily.bozek@gmail.com>
2022-04-07 21:38:09 -0700

    parameterized queries

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,13 +7,19 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-
-pool.query(`
+const args = process.argv.slice(2);
+const cohortName = args[0];
+const limit = args[1] || 5;
+const values = [`%{cohortName}%`, limit];
+const queryStatement = `
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
-LIMIT ${process.argv[3] || 5};
-`)
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`;
+
+pool.query(queryStatement, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
